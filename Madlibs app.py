cf1bfacd56c8e8cd877bf2b06cdf62796fbdc48d
Sabir Rohan Quazi <sabirrohanquazi@MacBook-Pro-som-tillhor-Sabir.local>
2020-02-11 10:35:01 +0100

    Fixed a nice Madlibs app with python !

---
+++ Madlibs app.py
@@ -0,0 +1,24 @@
+
+emotion = input("Enter a Emotion: ")
+emotion2 = input("Enter a Emotion: ")
+adjective = input("Enter a Adjective: ")
+number = input("Enter a Number: ")
+plural_noun2 = input("Enter a Plural Noun: ")
+plural_noun3 = input("Enter a Plural Noun: ")
+plural_noun4 = input("Enter a Plural Noun: ")
+body_part = input("Enter a Body Part: ")
+activity = input("Enter a Activity: ")
+food = input("Enter a Food: ")
+
+
+
+
+print("Getting ready for your new baby can make you feel " + emotion)
+print("and " + emotion2 + ".")
+print("Here are some " + adjective)
+print("tips to help you prepare. Pack your hospital bag at least " + number + " days before your due date.")
+print("Don't forget to pack your " + plural_noun2 + " and your " + plural_noun3 + ".")
+print("Before your baby arrives,baby proof every " + plural_noun4 + " in your house to protect your baby's sensitive " + body_part +".")
+print("Lastly, remember to " + activity + " together before the baby arrives.Follow these tips,and parenting is sure to be a piece of " + food +"!")
+
+
